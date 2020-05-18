c81ac5c9fc8289c0b5b522876893cb7c9034783d
Matthijs Kotterink <poukimonster@gmail.com>
2020-05-18 11:14:27 +0200

    Aanpassingen website

---
+++ Philips_MVC_Visual/Philips_MVC_Visual/Controllers/ChatController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Philips_MVC_Visual.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        public IActionResult CreateChat()
+        {
+            return View();
+        }
+    }
+}
---
+++ Philips_MVC_Visual/Philips_MVC_Visual/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Philips_MVC_Visual.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
