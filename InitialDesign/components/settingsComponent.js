dd8fcd364fca95ba377458372ae2f5941ba75a04
Luuk Knippenberg <luukknippenberg@derros.nl>
2020-05-28 16:34:36 +0200

    Settings Get Luuk

--- InitialDesign/components/settingsComponent.js
+++ InitialDesign/components/settingsComponent.js
@@ -3,14 +3,15 @@ import { View, Text, TextInput, Button } from "react-native";
 import Gun from 'gun/gun.js' 
 const gun = new Gun('https://diabetesappfontysgroep3.herokuapp.com/gun') 
 var weightSetting = gun.get('weight');
+var sugarSetting = gun.get('sugar');
 
 export default class SettingsComponent extends React.Component { 
 
     constructor(){
         super();
         this.state = {
-            weightState: '',
-            sugarState: ''
+            weightState: getWeight(),
+            sugarState: getSugar()
         };
     }
 
@@ -25,7 +26,6 @@ export default class SettingsComponent extends React.Component {
                     style={{height: 40}}
                     placeholder={getWeight()}
                     onChangeText={(weightState) => this.setState({weightState})}
-                    defaultValue={this.state.weightState}
                 />
                 <br></br>
                 <Text>Bloedsuikerspiegel doel</Text>
@@ -33,7 +33,7 @@ export default class SettingsComponent extends React.Component {
                     style={{height: 40}}
                     placeholder={getSugar()}
                     onChangeText={(sugarState) => this.setState({sugarState})}
-                    defaultValue={this.state.sugarState}
+                    
                 />
                     <br></br>
                 <Button
@@ -42,7 +42,7 @@ export default class SettingsComponent extends React.Component {
                 />
                 <Button
                     title="Annuleren"
-                    onPress={() => navigation.navigate('Home')}
+                    onPress={() => this.props.navigation.navigate('Home')}
                 />
             </View>
         )
@@ -51,23 +51,36 @@ export default class SettingsComponent extends React.Component {
 
 ///Get and set methods from the settings screen
 var sugar = 0;
+var weight = 0;
 
 function setWeight(data)
 {
-    weightSetting.set(data);
-    console.log(weightSetting)
+    if(data){
+        weightSetting.set(data);
+    }
+    
+    // console.log(weightSetting)
 }
 function setSugar(data)
 {
-    sugar = data
+    if(data){
+        sugarSetting.set(data);
+    }
 }
 
 function getWeight()
 {
-    // gun.get('weight').get('weight')
-    return "test";
+    weightSetting.on(function(item, id){
+        weight = item[Object.keys(item)[Object.keys(item).length - 1]]
+    })
+
+    return weight;
 }
 function getSugar()
 {
+    sugarSetting.on(function(item, id){
+        sugar = item[Object.keys(item)[Object.keys(item).length - 1]]
+    })
+    
     return sugar;
 }
